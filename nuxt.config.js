1d4128a22f735227fa056c3b96925d630f50a620
Jang Haemin <io@jhaemin.com>
2019-06-23 18:34:58 +0900

    Change title, add link tags, remove proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,31 +6,26 @@ export default {
       lang: 'ko',
       class: 'light-mode'
     },
-    title: process.env.npm_package_name || '',
+    title: 'J.Haemin | 장해민',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v2' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#333333' }
     ]
   },
   srcDir: 'src',
   loading: false,
   css: [
     '~/assets/css/fonts.css',
-    {
-      src: '~/assets/scss/globalstyle.scss',
-      lang: 'scss'
-    }
+    { src: '~/assets/scss/globalstyle.scss', lang: 'scss' }
   ],
   build: {
     publicPath: '/dist/'
-  },
-  proxy: {
-    '/': {
-      target: 'https://github.com'
-    }
   }
 }
