532b7a62367aa43608061ea4e10c4e88ceaa4494
Jang <io@jhaemin.com>
2019-06-24 20:01:43 +0900

    Move main tag's style to default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <GlobalNav />
-    <nuxt />
-    <GlobalFooter />
+  <div id="default-layout">
+    <GlobalNav/>
+    <nuxt class="main"/>
+    <GlobalFooter/>
   </div>
 </template>
 
@@ -11,6 +11,22 @@ import GlobalNav from '~/components/GlobalNav.vue'
 import GlobalFooter from '~/components/GlobalFooter.vue'
 
 export default {
-  components: {GlobalNav, GlobalFooter}
+  components: { GlobalNav, GlobalFooter }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/global-variables.scss';
+@import '~/assets/scss/global-mixins.scss';
+
+#default-layout {
+  .main {
+    // padding-top: $moderate-gap--vertical;
+    min-height: calc(100vh - #{$gn-height} - #{$gf-height});
+    width: calc(100% - #{2 * $moderate-gap});
+    max-width: 30rem;
+    margin: auto;
+    padding-bottom: $moderate-bottom;
+  }
+}
+</style>
