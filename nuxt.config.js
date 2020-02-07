ede9325416a5aae571172a36e1f8817d752908b8
Jang Haemin <io@jhaemin.com>
2020-02-06 23:55:21 -0500

    chore: remove the page transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   head: {
     htmlAttrs: {
       lang: 'ko',
-      class: 'light-mode'
+      class: 'light-mode',
     },
     title: title,
     meta: [
@@ -16,43 +16,43 @@ export default {
       { hid: 'description', name: 'description', content: description },
       {
         property: 'og:title',
-        content: title
+        content: title,
       },
       {
         property: 'og:description',
-        content: description
+        content: description,
       },
       {
         property: 'og:image',
-        content: 'https://jhaemin.com/og/jhaemin_open_graph_logo.png'
-      }
+        content: 'https://jhaemin.com/og/jhaemin_open_graph_logo.png',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v1' },
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/apple-touch-icon.png'
+        href: '/apple-touch-icon.png',
       },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#333333' }
-    ]
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#333333' },
+    ],
   },
   srcDir: 'src',
   router: {
     linkActiveClass: 'active-link',
-    linkExactActiveClass: 'exact-active-link'
+    linkExactActiveClass: 'exact-active-link',
   },
   loading: false,
   css: [
     '~/assets/css/fonts.css',
-    { src: '~/assets/scss/globalstyle.scss', lang: 'scss' }
+    { src: '~/assets/scss/globalstyle.scss', lang: 'scss' },
   ],
-  pageTransition: {
-    name: 'fade',
-    mode: 'out-in'
-  },
+  // pageTransition: {
+  //   name: 'fade',
+  //   mode: 'out-in'
+  // },
   build: {
-    publicPath: '/dist/'
-  }
+    publicPath: '/dist/',
+  },
 }
