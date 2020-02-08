d1ef88331f7a19a5e2a6885d11921749729c4cf5
Jang Haemin <io@jhaemin.com>
2020-02-08 16:28:07 -0500

    photography: add more photos

---
+++ src/components/photography/PhotoSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="photo-section">
+    <div>
+      <img :src="imgSrc" :alt="alt || name" class="photo" />
+      <caption class="caption">
+        <div class="name">{{ name }}</div>
+        <div class="info">{{ `${date} | ${device}` }}</div>
+      </caption>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    device: {
+      type: String,
+      default: 'iPhone',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    date: {
+      type: String,
+      default: '',
+    },
+    imgSrc: {
+      type: String,
+      default: '~/assets/images/photography/34th-st.jpeg',
+    },
+    alt: {
+      type: String,
+      default: 'image',
+    },
+    caption: {
+      type: String,
+      default: 'caption',
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/scss/global-variables';
+
+.photo-section {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 8rem;
+
+  .photo {
+    max-height: calc(100vh - 6rem);
+    max-width: 100%;
+    position: relative;
+  }
+
+  .caption {
+    display: block;
+    font-size: 0.8rem;
+    margin-top: 1rem;
+
+    .name {
+      font-weight: 600;
+    }
+
+    .info {
+      font-size: 0.75rem;
+      color: $universal-gray;
+      margin-top: 0.3rem;
+    }
+  }
+}
+</style>
--- src/pages/photography.vue
+++ src/pages/photography.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="photography">
     <div class="hero-new-york-city-wrapper">
-      <h1 class="title">Photography</h1>
+      <h1 class="title">
+        Photography
+      </h1>
       <img
         src="~/assets/images/photography/new-york-city.jpeg"
         alt="New York City"
@@ -9,51 +11,130 @@
       />
       <caption>New York City, February 3, 2020</caption>
     </div>
+
+    <PhotoSection
+      :img-src="require('~/assets/images/photography/34th-st.jpeg')"
+      :name="`34th St`"
+      :date="`January 31th, 2020`"
+      :device="`iPhone 8`"
+    />
+    <PhotoSection
+      :img-src="require(`~/assets/images/photography/jane's-carousel.jpeg`)"
+      :name="`Jane's Carousel`"
+      :date="`February 2nd, 2020`"
+      :device="`iPhone 8`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/under-the-brooklyn-bridge.jpeg`)
+      "
+      :name="`Under the Brooklyn Bridge`"
+      :date="`February 3rd, 2020`"
+      :device="`iPhone 8`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/schloss-neuschwanstein-july-03-2015.jpeg`)
+      "
+      :name="`Schloss Neuschwanstein`"
+      :date="`July 3rd, 2015`"
+      :device="`iPhone 5s`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/grand-canal-july-04-2015.jpeg`)
+      "
+      :name="`Grand Canal`"
+      :date="`July 4th, 2015`"
+      :device="`iPhone 5s`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/jungfrau-july-09-2015.jpeg`)
+      "
+      :name="`Jungfrau`"
+      :date="`July 9th, 2015`"
+      :device="`iPhone 5s`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/flowerpot-may-16-2018.jpeg`)
+      "
+      :name="`Flowerpot`"
+      :date="`May 16th, 2018`"
+      :device="`iPhone 5s`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/unknown-light-march-15-2019.jpeg`)
+      "
+      :name="`Unknown Light`"
+      :date="`March 15th, 2019`"
+      :device="`iPhone 8`"
+    />
+    <PhotoSection
+      :img-src="
+        require(`~/assets/images/photography/stairs-october-15-2019.jpeg`)
+      "
+      :name="`Stairs`"
+      :date="`October 15th, 2019`"
+      :device="`iPhone 8`"
+    />
   </div>
 </template>
+
+<script>
+import PhotoSection from '~/components/photography/PhotoSection'
+
+export default {
+  components: { PhotoSection },
+}
+</script>
 
 <style lang="scss">
 @import '~/assets/scss/global-mixins';
 
 #photography {
   user-select: none;
-}
+  background-color: #fff;
 
-.hero-new-york-city-wrapper {
-  width: 100%;
-  height: 100vh;
-  position: relative;
+  .hero-new-york-city-wrapper {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    margin-bottom: 8rem;
 
-  .title {
-    position: absolute;
-    left: 50%;
-    top: 20%;
-    color: #fff;
-    transform: translateX(-50%) translateY(-50%);
-    font-size: 5rem;
-    font-weight: 800;
-    letter-spacing: -0.02em;
-    // text-shadow: 0 0 2rem rgba(#000, 0.2);
+    .title {
+      position: absolute;
+      left: 50%;
+      top: 20%;
+      color: #fff;
+      transform: translateX(-50%) translateY(-50%);
+      font-size: 5rem;
+      font-weight: 800;
+      letter-spacing: -0.02em;
+      // text-shadow: 0 0 2rem rgba(#000, 0.2);
 
-    @include smaller-than(600px) {
-      font-size: 3rem;
+      @include smaller-than(600px) {
+        font-size: 3rem;
+      }
     }
-  }
 
-  .hero-new-york-city {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+    .hero-new-york-city {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 
-  caption {
-    width: 100%;
-    position: absolute;
-    left: 50%;
-    bottom: 1rem;
-    color: #fff;
-    transform: translateX(-50%);
-    font-size: 0.8rem;
+    caption {
+      width: 100%;
+      position: absolute;
+      left: 50%;
+      bottom: 1rem;
+      color: #fff;
+      transform: translateX(-50%);
+      font-size: 0.8rem;
+    }
   }
 }
 </style>
